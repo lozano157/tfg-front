5bad85e20fa01ff0689fd553934bbe21ca32de7e
Javier <javierlozanoolalla@gmail.com>
2024-10-20 14:50:59 +0200

    prueba vercel.json

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { fileURLToPath, URL } from 'node:url'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/', // Asegura que la base de las rutas sea correcta para Vercel
   plugins: [
     vue(),
     vueDevTools(),
